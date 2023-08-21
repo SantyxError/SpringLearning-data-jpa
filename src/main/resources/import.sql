b4e6824df45df1881d173abfc8f11c4f3214fddd
Santiago <sanbemal@gmail.com>
2023-08-21 09:21:29 +0200

    121. Creando la vista listar.html

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1,'Andres', 'Gúzman','profesor@udemy.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2,'Pedro', 'Manchón','alumno@udemy.com', '2019-02-22');
