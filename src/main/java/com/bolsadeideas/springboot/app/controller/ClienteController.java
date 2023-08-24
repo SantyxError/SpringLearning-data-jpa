95f587c237c2e3efebe6480f7dcbcce7084d281f
Santiago <sanbemal@gmail.com>
2023-08-24 08:41:22 +0200

    144. Bonus track: Paginador desde cero - Usando PagingAndSortingRepository - Parte I

--- src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controller/ClienteController.java
@@ -1,16 +1,19 @@
 package com.bolsadeideas.springboot.app.controller;
 
-import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,9 +28,12 @@ public class ClienteController {
 	private IClienteService clienteService;
 
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listar(Model model) {
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+		Pageable pageRequest = PageRequest.of(page, 4);
+
+		Page<Cliente> clientes = clienteService.findAll(pageRequest);
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteService.findAll());
+		model.addAttribute("clientes", clientes);
 		return "listar";
 	}
 
@@ -45,7 +51,7 @@ public class ClienteController {
 		Cliente cliente = null;
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
-			if (cliente == null){
+			if (cliente == null) {
 				flash.addFlashAttribute("error", "El id del cliente no existe en la BBDD");
 				return "redirect:/listar";
 			}
@@ -64,11 +70,11 @@ public class ClienteController {
 			model.addAttribute("titulo", "Formulario de Cliente");
 			return "form";
 		}
-		String mensajeFlash = (cliente.getId() !=null)? "Cliente editado con éxito!": "Cliente creado con éxito!";
+		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito!" : "Cliente creado con éxito!";
 
 		clienteService.save(cliente);
 		status.setComplete();
-		flash.addFlashAttribute("success",mensajeFlash);
+		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";
 	}
 
