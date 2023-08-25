2e6547f8a3bf0db7b1346f084914e6a4541c6e84
Santiago <sanbemal@gmail.com>
2023-08-25 14:51:59 +0200

    150. Configuraciones y preparando la aplicación

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -41,6 +41,8 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 
+	private String foto;
+
 	private static final long serialVersionUID = 1L;
 
 	public void setId(Long id) {
@@ -81,5 +83,13 @@ public class Cliente implements Serializable {
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 }
